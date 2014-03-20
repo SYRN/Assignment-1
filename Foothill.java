daa7b5d8c3097abfc4b40bb4090cd4dbe28b5aaf
Shlomo Nazarian <shlomoyrnazarian@gmail.com>
2014-03-19 18:18:16 -0700

    Added stuff

--- Foothill.java
+++ Foothill.java
@@ -4,6 +4,7 @@ public class Foothill
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
+      System.out.println("Hello World! of git");
       System.out.println("My family name is Nazarian.");
       System.out.println("My student ID is 20078413.");
       System.out.println("I curently reside in Palo Alto.");
