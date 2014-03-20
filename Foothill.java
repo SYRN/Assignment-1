c1573cab5ea798ba5576d9662f1ce5f6fe8e35b5
Shlomo Nazarian <shlomoyrnazarian@gmail.com>
2014-03-19 18:14:19 -0700

    The original commit for the password project

---
+++ Foothill.java
@@ -0,0 +1,26 @@
+
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      System.out.println("My family name is Nazarian.");
+      System.out.println("My student ID is 20078413.");
+      System.out.println("I curently reside in Palo Alto.");
+      System.out.println("The syllabus says that every student must post " + 
+         "at least one question, comment or answer in the public forum " + 
+         "every week (in addition to the introduction post).");
+   }
+}
+
+/* ---------------- Paste From Console  --------------------------------
+
+Hello World!
+My family name is Nazarian.
+My student ID is 20078413.
+I curently reside in Palo Alto.
+The syllabus says that every student must post at least one question, comment or
+ answer in the public forum every week (in addition to the introduction post).
+
+
+------------------------------------------------------------- */
